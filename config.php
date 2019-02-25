8b16972ad436357447b9768cb82f3ba2ac38aac8
tmurvv <tmurv@shaw.ca>
2019-02-25 16:04:52 -0700

    init commit

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
  //email info for contact form
- //$email = 'tmurv@shaw.ca'; //for testing
+ //$email = 'tmurv@shaw.ca';
  $email = 'info@albertaharpist.com';
  $siteEmailRecipient = "Tiffany";
 
- //reCaptcha ver. 3 info for contact form
- $recaptchaKey = '6LdrkJIUAAAAAItgLonuFmpJSDOnofRZqEaamBni';
- $recaptchaSecretKey = '6LdrkJIUAAAAAMfuPB0mkRBec5fj2H2I3RlXtXku';
+ //reCaptcha info for contact form
+ $recaptchaKey = '';
+ $recaptchaSecretKey = '';
